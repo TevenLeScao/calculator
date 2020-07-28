6dd888fd0e339c44de527d9dac08bc58bbd1e779
TevenLeScao <teven.lescao@gmail.com>
2020-07-28 10:54:03 +0200

    added train_tokenizer.py

---
+++ train_tokenizer.py
@@ -0,0 +1,8 @@
+import tokenizers
+from os import path, listdir
+prefix = path.join("../deepmind-gutenberg/train_concat")
+files = [path.join(prefix, file_name) for file_name in listdir(prefix)]
+tokenizer = tokenizers.SentencePieceBPETokenizer()
+tokenizer.train(files, vocab_size=40000)
+tokenizer.save_model("tokenizer_pg19")
+
