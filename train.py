1bbd4f9048aba88ed1012c7c64934c7fa58bbfdf
TevenLeScao <teven.lescao@gmail.com>
2020-07-29 16:55:43 +0200

    convert_to_features + changed tokenizer to ByteLevelBPE

--- train.py
+++ train.py
@@ -1,41 +1,78 @@
+import os
+import argparse
+import itertools
+
 import nlp
 from transformers import DataCollatorForLanguageModeling, LineByLineTextDataset, TrainingArguments, Trainer, \
     GPT2Config, GPT2Tokenizer, GPT2LMHeadModel
-import argparse
 
 
-
-def convert_to_features(example_batch):
+# 2048 was GPT3's context size
+def convert_to_features(example_batch, ids, context_size=2048, cut_trail_off=True):
     # Tokenize contexts and questions (as pairs of inputs)
     batch_text = example_batch['text']
     encodings = tokenizer.batch_encode_plus(batch_text, pad_to_max_length=False)
+    input_ids = encodings["input_ids"]
+    attention_masks = encodings["attention_mask"]
+    encodings["external_ids"] = list(itertools.chain.from_iterable(
+        [[doc_idx] * (len(text_ids) // context_size) for doc_idx, text_ids in zip(ids, input_ids)])
+    )
+    encodings["internal_ids"] = list(itertools.chain.from_iterable(
+        [range(len(text_ids) // context_size) for text_ids in input_ids])
+    )
+    if cut_trail_off:
+        input_ids = [text_ids[i:i + context_size]
+                     for text_ids in input_ids
+                     for i in range(0, len(text_ids), context_size)[:-1]]
+        attention_masks = [masks[i:i + context_size]
+                           for masks in attention_masks
+                           for i in range(0, len(masks), context_size)[:-1]]
+    else:
+        input_ids = [text_ids[i:i + context_size]
+                     for text_ids in input_ids
+                     for i in range(0, len(text_ids), context_size)]
+        attention_masks = [masks[i:i + context_size]
+                           for masks in attention_masks
+                           for i in range(0, len(masks), context_size)]
+    encodings["input_ids"] = input_ids
+    encodings["attention_mask"] = attention_masks
     return encodings
+
+
+def chunk_examples(examples):
+    chunks = []
+    for sentence in examples['sentence1']:
+        chunks += [sentence[i:i + 50] for i in range(0, len(sentence), 50)]
+    return {'chunks': chunks}
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--remap", action="store_true")
     parser.add_argument("--sanity", action="store_true")
     args = parser.parse_args()
-    print(f"sanity is {args.sanity}")
-    
+    for k, v in vars(args).items():
+        print(f"{k}: {v}")
+
     cache_file_name = "data/tokenized_dataset" + ("_sanity" if args.sanity else "") + ".pyarrow"
     # Model
     config = GPT2Config(
-        vocab_size=40001, n_layer=6,
+        vocab_size=40000, n_layer=6,
     )
-    tokenizer = GPT2Tokenizer.from_pretrained("tokenizer_pg19", max_len=512, pad_token="<pad>")
+    tokenizer = GPT2Tokenizer.from_pretrained("tokenizer_pg19")
     model = GPT2LMHeadModel(config=config)
 
-    try:
+    if os.path.isfile(cache_file_name) and not args.remap:
         train_set = nlp.Dataset.from_file(cache_file_name)
-    except FileNotFoundError:
+    else:
         # Data
         if args.sanity:
             train_set = nlp.load_dataset('pg19', split='train[:1%]')
         else:
             train_set = nlp.load_dataset('pg19', split='train')
-        train_set = train_set.map(convert_to_features, batched=True, batch_size=10, cache_file_name=cache_file_name,
-                                  remove_columns=["text"])
+        train_set = train_set.map(convert_to_features, with_indices=True, batched=True, batch_size=10,
+                                  cache_file_name=cache_file_name, remove_columns=train_set.column_names)
+    train_set.set_format("torch")
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
     # Trainer
